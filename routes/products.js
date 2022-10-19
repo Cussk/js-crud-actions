dbc3881362e7f32f3dcbb22eda288959f59d7ce0
Kyle Cuss <cussprogramming@gmail.com>
2022-10-19 08:14:21 -0230

    created CRUD actions for API

--- routes/products.js
+++ routes/products.js
@@ -1,6 +1,33 @@
 var express = require("express");
+const { route } = require("./client");
 var router = express.Router();
 
 module.exports = function (db) {
+  //Read function
+  router.route("/products")
+  .get((req, res) => {
+    res.send(db.get("products").value());
+  })
+  //Create function
+  .post( (req, res) => {
+    const newProduct = req.body;
+    res.send(db.get("products").insert(newProduct).write());
+  });
+  router.route("/products/:id")
+  //update function
+  .patch((req,res) => {
+    res.send(db.get("products").find({id: req.params.id}).assign(req.body).write());
+  })
+  //delete function
+  .delete((req,res) => {
+    db.get("products").remove({id: req.params.id}).write();
+    res.status(204).send();
+  })
+  //find function
+  .get((req,res) => {
+    res.send(
+      db.get("products").find({id: req.params.id}).value());
+  });
+
   return router;
 };
