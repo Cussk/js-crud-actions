b3ee568fdaa5ab5bb14dbd3400209112151ad4b4
Kyle Cuss <cussprogramming@gmail.com>
2022-10-20 18:12:09 -0230

    Enabled single keyword search and detailed search

--- routes/products.js
+++ routes/products.js
@@ -1,8 +1,9 @@
 var express = require("express");
-const { route } = require("./client");
 var router = express.Router();
+const qs = require("qs");
 
 module.exports = function (db) {
+
   //Read function
   router.route("/products")
   .get((req, res) => {
@@ -13,6 +14,44 @@ module.exports = function (db) {
     const newProduct = req.body;
     res.send(db.get("products").insert(newProduct).write());
   });
+
+  //search single keyword
+  router.route("/products/search").get((req, res) => {
+    const keywords = req.query.keywords.split(" ");
+    const result = db.get("products").filter((_) => {
+      const fullText = _.description + _.name + _.color;
+
+      return keywords.every((_) => fullText.indexOf(_) !== -1);
+    });
+
+    res.send(result);
+  });
+
+    // search multiple keywords
+    router.route("/products/detailSearch").get((req, res) => {
+      const query = qs.parse(req.query);
+  
+      const results = db.get("products").filter((_) => {
+        return Object.keys(query).reduce((found, key) => {
+          const obj = query[key];
+          switch (obj.op) {
+            case "lt":
+              found = found && _[key] < obj.val;
+              break;
+            case "eq":
+              found = found && _[key] == obj.val;
+              break;
+            default:
+              found = found && _[key].indexOf(obj.val) !== -1;
+              break;
+          }
+          return found;
+        }, true);
+      });
+
+      res.send(results);
+    });
+
   router.route("/products/:id")
   //update function
   .patch((req,res) => {
